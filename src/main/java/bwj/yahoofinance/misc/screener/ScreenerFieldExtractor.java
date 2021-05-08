964727b0eea0fbac0141556b1b82b5e6a02fdf61
Brad Jacobs <brad.jacobs@theplatform.com>
2021-05-07 21:37:58 -0700

    clean up json parsing within the screener defn extractor

--- src/main/java/bwj/yahoofinance/misc/screener/ScreenerFieldExtractor.java
+++ src/main/java/bwj/yahoofinance/misc/screener/ScreenerFieldExtractor.java
@@ -2,10 +2,8 @@ package bwj.yahoofinance.misc.screener;
 
 import bwj.yahoofinance.misc.screener.autogen.Category;
 import bwj.yahoofinance.misc.screener.autogen.ScreenerField;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
+import bwj.yahoofinance.util.JsonDataExtractor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.io.IOException;
 import java.net.URL;
@@ -25,6 +23,8 @@ public class ScreenerFieldExtractor
     // note: url copied from web (haven't analyzed what all the params mean)
     private static final String FIELD_DEFN_QUERY = "https://query1.finance.yahoo.com/v1/finance/screener/instrument/equity/fields?lang=en-US&region=US&category=keystats%2Cfinancials%2Cvaluation%2Csector_industry%2Cesgscores%2Cincome%2Ccashflowstatement%2Cbalance_sheet%2Cearnings%2Cdividends_and_splits%2Cprofile%2Cfair_value%2Cpopular_filters%2Cchanges_in_price_and_market_cap%2Cchanges_in_volume_and_ownership%2Cvaluation_metric%2Cprofitability_ratios_and_dividends%2Cdebt_ratios%2Cliquidity_ratios%2Ceps_and_income_statement%2Ccash_flow%2Cesg_scores%2Cshort_interest%2Cfair_value&corsDomain=finance.yahoo.com";
 
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     public static void main(String[] args) throws Exception
     {
         ScreenerFieldExtractor sfe = new ScreenerFieldExtractor();
@@ -35,18 +35,13 @@ public class ScreenerFieldExtractor
     {
         String json = urlRead(FIELD_DEFN_QUERY);
 
-        ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
-        JsonNode root = mapper.readTree(json);
+        // extract inner map of maps
+        JsonDataExtractor jsonDataExtractor = new JsonDataExtractor(json);
+        Map<String, Map<String, Object>> mapOfMaps = jsonDataExtractor.parseMapOfMaps("/finance/result/0/fields");
 
-        JsonNode innerNode = root.required("finance").required("result").required(0).required("fields");
-
-        // NOTE: i'm sure there's a simpler way to extract the data
-        String innerJsonString = innerNode.toString();
-
-        // convert to map of map objects
-        Map<String, Map<String, Object>> mapOfMaps = mapper.readValue(innerJsonString, new TypeReference<Map<String, Map<String, Object>>>() {});
         // the key name is the same as the fieldId, thus we only need all the "values" from the collection.
         List<Map<String, Object>> listOfMaps = new ArrayList<>(mapOfMaps.values());
+
 
         ScreenerField[] fields = mapper.convertValue(listOfMaps, ScreenerField[].class);
 
@@ -55,7 +50,6 @@ public class ScreenerFieldExtractor
                 .filter(sf -> !sf.getDeprecated())
                 .filter(sf -> sf.getIsPremium())
                 .collect(Collectors.toList());
-
 
 
         // filter out fields want to ignore.
