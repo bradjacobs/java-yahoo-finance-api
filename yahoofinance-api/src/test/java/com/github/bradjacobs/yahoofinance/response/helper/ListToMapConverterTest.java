3f5e07e588b71deedb0ae4c2af59e5974b9798f5
bradjacobs <bradjacobs@gmail.com>
2021-07-25 21:35:31 -0700

    added some basic unittests for helper class.

---
+++ yahoofinance-api/src/test/java/com/github/bradjacobs/yahoofinance/response/helper/ListToMapConverterTest.java
@@ -0,0 +1,87 @@
+package com.github.bradjacobs.yahoofinance.response.helper;
+
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.testng.Assert.*;
+
+public class ListToMapConverterTest
+{
+    private static final String DATE_KEY = "date";
+    private static final String INT_KEY = "intValue";
+    private static final String STRING_KEY = "stringValue";
+
+    @Test
+    public void testSortedMapHappyPath() throws Exception
+    {
+        Map<String, Object> map1 = createTestEntityMap("2021-02-01", 55, "cat");
+        Map<String, Object> map2 = createTestEntityMap("2021-01-01", 44, "dog");
+        Map<String, Object> map3 = createTestEntityMap("2021-03-01", 77, "cow");
+
+        Map<String, Map<String, Object>> expectedMap = new TreeMap<>();
+        expectedMap.put((String)map2.get(DATE_KEY), map2);
+        expectedMap.put((String)map1.get(DATE_KEY), new TreeMap<>(map1));  // check that really equals, not jsut identity equals.
+        expectedMap.put((String)map3.get(DATE_KEY), map3);
+
+        List<Map<String, Object>> inputList = Arrays.asList(map1, map2, map3);
+
+        Map<String, Map<String, Object>> resultMap = ListToMapConverter.convertToMap(DATE_KEY, inputList, true);
+        assertNotNull(resultMap);
+        assertEquals(resultMap.size(), 3, "mismatch expected map size");
+
+        List<String> dateKeys = new ArrayList<>(resultMap.keySet());
+        assertEquals("2021-01-01", dateKeys.get(0));
+        assertEquals("2021-02-01", dateKeys.get(1));
+        assertEquals("2021-03-01", dateKeys.get(2));
+
+        assertEquals(expectedMap, resultMap);
+    }
+
+
+    @Test
+    public void testInsertOrderMapHappyPath() throws Exception
+    {
+        Map<String, Object> map1 = createTestEntityMap("2021-02-01", 55, "cat");
+        Map<String, Object> map2 = createTestEntityMap("2021-01-01", 44, "dog");
+        Map<String, Object> map3 = createTestEntityMap("2021-03-01", 77, "cow");
+
+        Map<String, Map<String, Object>> expectedMap = new LinkedHashMap<>();
+        expectedMap.put((String)map1.get(DATE_KEY), map1);
+        expectedMap.put((String)map2.get(DATE_KEY), new TreeMap<>(map2));  // check that really equals, not jsut identity equals.
+        expectedMap.put((String)map3.get(DATE_KEY), map3);
+
+        List<Map<String, Object>> inputList = Arrays.asList(map1, map2, map3);
+
+        Map<String, Map<String, Object>> resultMap = ListToMapConverter.convertToMap(DATE_KEY, inputList, false);
+        assertNotNull(resultMap);
+        assertEquals(resultMap.size(), 3, "mismatch expected map size");
+
+        List<String> dateKeys = new ArrayList<>(resultMap.keySet());
+        assertEquals("2021-02-01", dateKeys.get(0));
+        assertEquals("2021-01-01", dateKeys.get(1));
+        assertEquals("2021-03-01", dateKeys.get(2));
+
+        assertEquals(expectedMap, resultMap);
+    }
+
+
+
+
+
+    private  Map<String, Object> createTestEntityMap(String date, int inValue, String strValue)
+    {
+        Map<String, Object> map = new HashMap<>();
+        map.put(DATE_KEY, date);
+        map.put(INT_KEY, inValue);
+        map.put(STRING_KEY, strValue);
+        return map;
+    }
+
+}
