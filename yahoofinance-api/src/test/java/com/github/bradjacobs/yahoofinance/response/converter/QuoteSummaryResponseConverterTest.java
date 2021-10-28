00ac8f77bb29a592e0cd478d6d51f437d6b0a757
bradjacobs <bradjacobs@gmail.com>
2021-10-28 14:55:11 -0700

    update quotesummary converter testcase to confirm bug fix

--- yahoofinance-api/src/test/java/com/github/bradjacobs/yahoofinance/response/converter/QuoteSummaryResponseConverterTest.java
+++ yahoofinance-api/src/test/java/com/github/bradjacobs/yahoofinance/response/converter/QuoteSummaryResponseConverterTest.java
@@ -15,38 +15,47 @@ public class QuoteSummaryResponseConverterTest
     private static final QuoteSummaryResponseConverter converter = new QuoteSummaryResponseConverter();
 
     @Test
-    public void testConvertToMap() throws Exception
+    public void testConvertMultiModulesToMap() throws Exception
     {
         List<String> expectedModules =
                 Arrays.asList("cashflowStatementHistory", "balanceSheetHistory",
                         "incomeStatementHistory","incomeStatementHistoryQuarterly",
                         "financialData");
 
+        String responseJson = ResourceUtil.readResourceFileAsString("msft_quote_summary_multi_modules.json");
+        Map<String, Map<String, Object>> moduleMaps = converter.convertToMapOfMaps(responseJson);
 
-//        String responseJson = ResourceUtil.readResourceFileAsString("msft_quote_summary_multi_modules.json");
+        // must refer to the test json input to determine 'expected results'
+        assertNotNull(moduleMaps);
+        assertEquals(moduleMaps.size(), expectedModules.size());
+
+        for (String expectedModule : expectedModules) {
+            assertTrue(moduleMaps.containsKey(expectedModule), "Could not find expected module in map: " + expectedModule);
+        }
+    }
+
+    @Test
+    public void testConvertAllModulesToMap() throws Exception
+    {
         String responseJson = ResourceUtil.readResourceFileAsString("msft_quote_summary_all_modules.json");
         Map<String, Map<String, Object>> moduleMaps = converter.convertToMapOfMaps(responseJson);
 
         // must refer to the test json input to determine 'expected results'
         assertNotNull(moduleMaps);
-        //assertEquals(moduleMaps.size(), expectedModules.size());
 
-        for (String expectedModule : expectedModules) {
-            assertTrue(moduleMaps.containsKey(expectedModule), "Could not find expected module in map: " + expectedModule);
-        }
+        Map<String, Object> summaryDetailMap = moduleMaps.get("summaryDetail");
+        Map<String, Object> industryTrendMap = moduleMaps.get("industryTrend");
+        assertNotNull(summaryDetailMap);
+        assertNotNull(industryTrendMap);
+
+        // check that industryTrendMap does have a peRatio, but with a 'null' value
+        assertTrue(industryTrendMap.containsKey("peRatio"));
+        Object peRatioValue = industryTrendMap.get("peRatio");
+        assertNull(peRatioValue);
+
 
         JsonMapper prettyMapper = JsonMapperSingleton.getPrettyInstance();
-
         String pJson = prettyMapper.writeValueAsString(moduleMaps);
-
-
-        // "cashflowStatementHistory" -> {LinkedHashMap@1909}  size = 2
-        //"balanceSheetHistory" -> {LinkedHashMap@1911}  size = 2
-        //"incomeStatementHistory" -> {LinkedHashMap@1913}  size = 2
-        //"financialData" -> {LinkedHashMap@1915}  size = 30
-
-        int kddjk = 333;
-
     }
 
 }
