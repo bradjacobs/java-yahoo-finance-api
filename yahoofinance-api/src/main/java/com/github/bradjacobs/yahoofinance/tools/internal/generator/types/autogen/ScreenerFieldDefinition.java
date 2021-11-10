f59ae2882e1a6e533fec71004e8bda4d9019dd8e
bradjacobs <bradjacobs@gmail.com>
2021-11-09 16:09:45 -0800

    first round refactor of ScreenerField enum generation.

--- yahoofinance-api/src/main/java/com/github/bradjacobs/yahoofinance/tools/internal/generator/types/autogen/ScreenerFieldDefinition.java
+++ yahoofinance-api/src/main/java/com/github/bradjacobs/yahoofinance/tools/internal/generator/types/autogen/ScreenerFieldDefinition.java
@@ -3,12 +3,12 @@
  */
 package com.github.bradjacobs.yahoofinance.tools.internal.generator.types.autogen;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.TreeMap;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
@@ -48,6 +48,10 @@ public class ScreenerFieldDefinition implements Comparable<ScreenerFieldDefiniti
     private Boolean dropdownSupported;
     @JsonProperty("isPremium")
     private Boolean isPremium;
+
+    @JsonIgnore
+    private final Map<String, Object> additionalProperties = new TreeMap<>(); // holds any extra unrecognized entries.
+
 
     @JsonProperty("fieldId")
     public String getFieldId() {
@@ -178,4 +182,17 @@ public class ScreenerFieldDefinition implements Comparable<ScreenerFieldDefiniti
     public int hashCode() {
         return Objects.hash(fieldId);
     }
+
+    @JsonAnyGetter
+    private Map<String, Object> getAdditionalProperties()
+    {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    private void setAdditionalProperty(String name, Object value)
+    {
+        this.additionalProperties.put(name, value);
+    }
+
 }
